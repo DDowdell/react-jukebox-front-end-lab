cf7e8025652001d83ed2e50d938b5550667f6b1c
Dae Dowdell <dow_dell2010@yahoo.com>
2025-08-17 21:47:50 -0400

    set state and displaying data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
-import './App.css'
+// import './App.css'
+// import React from 'react';
+import { useState, useEffect } from 'react';
+import * as trackService from './services/trackService.js'
+import TrackList from './components/TrackList/TrackList.jsx';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const fetchedTracks = await trackService.index();
+        if (fetchedTracks.err) {
+          throw new Error(fetchedTracks.err);
+        }
+        setTracks(fetchedTracks);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchTracks();
+  }, []);
+
+
+  return (
+    <>
+      <TrackList tracks={tracks} />
+    </>
+  )
 };
 
 export default App;
---
+++ src/components/TrackList/TrackList.jsx
@@ -0,0 +1,12 @@
+const TrackList = (props) => {
+//   console.log(props);
+
+  return (
+    <div>
+      <h1>Track List</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default TrackList;
